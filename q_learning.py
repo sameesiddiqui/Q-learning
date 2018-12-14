a2e6232ceee72b08a57bb0a28b82e14597f6bd51
Samee Siddiqui <sameezsiddiqui@gmail.com>
2018-12-13 20:56:58 -0500

    Add discounted q_table

--- q_learning.py
+++ q_learning.py
@@ -144,10 +144,12 @@ def greedy_4x4_bot(curr_state):
 
     return action
 
-# learn the q_table
-# q_table = learn_q_table('FrozenLake-v0', 0.1, 1, 0.1)
+# learn the q_tables
+# uncomment to learn new q tables. make sure to comment out hard_codes q_tables below
+# q_table = learn_q_table('FrozenLake-v0', learning_rate=0.1, discount_factor=1, random_factor=0.1)
+# q_table_discounted = learn_q_table('FrozenLake-v0', learning_rate=0.1, discount_factor=0.95, random_factor=0.1)
 
-# learned q_table after 100k episodes
+# learned q_table with no discount after 100k episodes
 # uncomment if you don't want to train
 q_table = np.array([[0.38689906, 0.37441442, 0.37631358, 0.36515488],
  [0.24115092, 0.19194264, 0.26814398, 0.31177541],
@@ -166,6 +168,26 @@ q_table = np.array([[0.38689906, 0.37441442, 0.37631358, 0.36515488],
  [0.65959693, 0.85742038, 0.76518557, 0.73468326],
  [0.        , 0.        , 0.        , 0.        ]])
 
+# learned q_table with discount=0.95 after 100k episodes
+# uncomment if you don't want to train
+q_table_discounted = np.array([[0.11086028, 0.10873646, 0.11937905, 0.10764452],
+ [0.05457366, 0.0521126 , 0.06920769, 0.10343085],
+ [0.12007901, 0.07751225, 0.08306081, 0.08510609],
+ [0.06058289, 0.05500062, 0.05100563, 0.08659944],
+ [0.16391285, 0.09694021, 0.11711073, 0.10711821],
+ [0.        , 0.        , 0.        , 0.        ],
+ [0.10753944, 0.16301227, 0.05027957, 0.04267554],
+ [0.        , 0.        , 0.        , 0.        ],
+ [0.10461931, 0.16113925, 0.120788  , 0.19534531],
+ [0.20950422, 0.34757397, 0.21332139, 0.15570236],
+ [0.40224346, 0.26028362, 0.31256761, 0.15306386],
+ [0.        , 0.        , 0.        , 0.        ],
+ [0.        , 0.        , 0.        , 0.        ],
+ [0.21537173, 0.31379267, 0.49150142, 0.38470216],
+ [0.46608636, 0.78706054, 0.6564961 , 0.47320564],
+ [0.        , 0.        , 0.        , 0.        ]])
+
 play_game('FrozenLake-v0', q_table, ai='q_learning')
+play_game('FrozenLake-v0', q_table_discounted, ai='q_learning')
 play_game('FrozenLake-v0', ai='greedy')
 play_game('FrozenLake-v0', ai='random')
